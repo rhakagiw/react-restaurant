866db71a157f8966480b6a6151bf550485bd226e
Rhakagi <rhakagiw@gmail.com>
2021-03-15 19:04:09 -0400

    pushing build files

---
+++ build/precache-manifest.32aa1e970e28219c9c922921c0fc607d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf4d3b6ae31f888669174be883a326b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b196a26e78d0b154296",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fe5d61af2fb576c86b4b",
+    "url": "/static/css/main.aad77bbf.chunk.css"
+  },
+  {
+    "revision": "1b196a26e78d0b154296",
+    "url": "/static/js/2.f911962c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f911962c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5d61af2fb576c86b4b",
+    "url": "/static/js/main.88e5bc1c.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  }
+]);
