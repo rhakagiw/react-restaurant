4fc507382e97d296e0c7a2eb76e93510f08d186f
Rhakagi <rhakagiw@gmail.com>
2021-03-15 19:19:17 -0400

    Updates

---
+++ precache-manifest.9681ab9c1b125e603fffb8e26a4060f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f400021d1695d49e4030a3772fc3bbde",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "1b196a26e78d0b154296",
+    "url": "/react-restaurant/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fe5d61af2fb576c86b4b",
+    "url": "/react-restaurant/static/css/main.aad77bbf.chunk.css"
+  },
+  {
+    "revision": "1b196a26e78d0b154296",
+    "url": "/react-restaurant/static/js/2.f911962c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-restaurant/static/js/2.f911962c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5d61af2fb576c86b4b",
+    "url": "/react-restaurant/static/js/main.88e5bc1c.chunk.js"
+  },
+  {
+    "revision": "5ec792e009fecb4db311",
+    "url": "/react-restaurant/static/js/runtime-main.d04cff4c.js"
+  }
+]);
