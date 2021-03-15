2122fe4addff5cb8e56ab38c51da40d31c9328f0
Rhakagi <rhakagiw@gmail.com>
2021-03-15 18:59:45 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./components/home";
+import Menu from "./components/menu";
+import Banquet from "./components/banquet";
+import Culinary from "./components/culinary";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+         <Header/>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/menu" component={Menu} />
+            <Route path="/banquet" component={Banquet} />
+            <Route path="/culinary" component={Culinary} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+          <Footer/>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
